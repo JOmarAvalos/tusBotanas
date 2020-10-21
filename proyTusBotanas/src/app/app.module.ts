40da3f4f791cd2add49672c5e80775fd4f513ab4
Omar Avalos <omar.avalos.m@gmail.com>
2020-10-20 19:20:47 -0500

    Estructura v1

--- proyTusBotanas/src/app/app.module.ts
+++ proyTusBotanas/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RedesComponent } from './components/shared/redes/redes.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RedesComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
