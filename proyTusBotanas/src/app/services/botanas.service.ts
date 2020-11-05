662c8a0e71de6eeefa7a1dc55e85130ceecf70ca
Christian Ortega <skywblack@gmail.com>
2020-11-04 19:46:45 -0600

    Portafolio V final
    Ya con todas las imagenes del portafolio y con cambios para subirlo a prod

--- proyTusBotanas/src/app/services/botanas.service.ts
+++ proyTusBotanas/src/app/services/botanas.service.ts
@@ -14,8 +14,32 @@ private botanas: Botana[] =
     img: 'assets/libs/img/tus_cacahuates_imagen.jpg',
     portafolio: [
       {
-        imgPortafolio: '',
-        txtPortafolio: ''
+        imgPortafolio: 'assets/libs/img/portfolio/japones_s1.jpg',
+        txtPortafolio: 'Japoneses'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/enchilados2.jpg',
+        txtPortafolio: 'Enchilados'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/españoles.jpg',
+        txtPortafolio: 'Españoles'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/hot2.jpg',
+        txtPortafolio: 'Hot'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/tus_cacahuates_garapiñados_ajon.jpg',
+        txtPortafolio: 'Garapiñados con Ajonjoli'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/tus_cacahuates_garapiñados_originales.jpg',
+        txtPortafolio: 'Garapiñados Originales'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/tus_cacahuates_salados.jpg',
+        txtPortafolio: 'Salados'
       }
     ]
   },
@@ -26,8 +50,32 @@ private botanas: Botana[] =
     img: 'assets/libs/img/tus_combinaciones_imagen.jpg',
     portafolio: [
       {
-        imgPortafolio: '',
-        txtPortafolio: ''
+        imgPortafolio: 'assets/libs/img/portfolio/cobinacionfuego_triangulos.jpg',
+        txtPortafolio: 'Fuego'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/comacho2.jpg',
+        txtPortafolio: 'Macho'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/comespecial2.jpg',
+        txtPortafolio: 'Especial'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/comeuropeo2.jpg',
+        txtPortafolio: 'Europeo'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/comexicano1.jpg',
+        txtPortafolio: 'Mexicano'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/comselecto2.jpg',
+        txtPortafolio: 'Selecto'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/comajo2.jpg',
+        txtPortafolio: 'Ajo'
       }
     ]
   },
@@ -38,8 +86,20 @@ private botanas: Botana[] =
     img: 'assets/libs/img/tus_semillas_imagen.jpg',
     portafolio: [
       {
-        imgPortafolio: '',
-        txtPortafolio: ''
+        imgPortafolio: 'assets/libs/img/portfolio/tusemillasgar.jpg',
+        txtPortafolio: 'Garbanzo'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/tusemillashaba.jpg',
+        txtPortafolio: 'Haba'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/tusemillaspepita.jpg',
+        txtPortafolio: 'Pepita'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/tusemillaspepitarusa.jpg',
+        txtPortafolio: 'Pepita Rusa'
       }
     ]
   },
@@ -50,8 +110,40 @@ private botanas: Botana[] =
     img: 'assets/libs/img/tus_gomitas_clasicas_imagen.jpg',
     portafolio: [
       {
-        imgPortafolio: '',
-        txtPortafolio: ''
+        imgPortafolio: 'assets/libs/img/portfolio/gomita_clasica_aros_durazno.jpg',
+        txtPortafolio: 'Aros de Durazno'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/gomita_clasica_gotitas.jpg',
+        txtPortafolio: 'Clásicas'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/gomita_clasica_lombriz.jpg',
+        txtPortafolio: 'Lombriz'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/gomita_clasica_aros_manzana.jpg',
+        txtPortafolio: 'Aros de Manzana'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/gomita_clasica_dientitos.jpg',
+        txtPortafolio: 'Dientitos'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/gomita_clasica_panditas.jpg',
+        txtPortafolio: 'Panditas'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/gomita_clasica_frutitas.jpg',
+        txtPortafolio: 'Frutitas'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/gomita_clasica_pingüinos.jpg',
+        txtPortafolio: 'Pingüinos'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/gomita_clasica_tiburones.jpg',
+        txtPortafolio: 'Tiburones'
       }
     ]
   },
@@ -62,8 +154,40 @@ private botanas: Botana[] =
     img: 'assets/libs/img/tus_gomitas_dluxe_imagen.jpg',
     portafolio: [
       {
-        imgPortafolio: '',
-        txtPortafolio: ''
+        imgPortafolio: 'assets/libs/img/portfolio/gomita_dluxe_cereza.jpg',
+        txtPortafolio: 'Cereza'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/gomita_dluxe_fresas_con_crema.jpg',
+        txtPortafolio: 'Fresas con crema'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/gomita_dluxe_guanabana.jpg',
+        txtPortafolio: 'Guanabana'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/gomita_dluxe_guayaba.jpg',
+        txtPortafolio: 'Guayaba'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/gomita_dluxe_mandarina.jpg',
+        txtPortafolio: 'Mandarina'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/gomita_dluxe_maracuya.jpg',
+        txtPortafolio: 'Maracuya'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/gomita_dluxe_menta_limon.jpg',
+        txtPortafolio: 'Menta Limón'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/gomita_dluxe_pai_de_limon.jpg',
+        txtPortafolio: 'Pay de limón'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/gomita_dluxe_toronja.jpg',
+        txtPortafolio: 'Toronja'
       }
     ]
   },
@@ -86,8 +210,24 @@ private botanas: Botana[] =
     img: 'assets/libs/img/tus_botanas_sanas.jpg',
     portafolio: [
       {
-        imgPortafolio: '',
-        txtPortafolio: ''
+        imgPortafolio: 'assets/libs/img/portfolio/botanas_sanas_arandano.jpg',
+        txtPortafolio: 'Arándano Granola y Nuez'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/botanas_sanas_arandano_morado.jpg',
+        txtPortafolio: 'Arándano Nuez'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/botanas_sanas_arandano_pasas_y_nuez_roja.jpg',
+        txtPortafolio: 'Arándano con pasas y nuez'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/botanas_sanas_arandano_verde.jpg',
+        txtPortafolio: 'Arándano Frutos secos'
+      },
+      {
+        imgPortafolio: 'assets/libs/img/portfolio/botanas_sanas_energy.jpg',
+        txtPortafolio: 'Energy'
       }
     ]
   },
